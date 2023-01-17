ee49b3f0454524673d66c0fe225a5e771bfdc010
shanec0 <connors.shane@hotmail.com>
2023-01-17 10:30:49 -0500

    need to make my parsers return the matched characters

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let sample = String::from("Hello, world");
 
     let hello = c_string("Hello,");
-    let world = c_string("world");
+    let world = c_string(" world");
     let c = c_sequence(vec![hello, world]);
 
 
@@ -11,35 +11,37 @@ fn main() {
 
     let result = c(&sample);
     match result {
-        Ok((x, _)) => println!("OK {}", x),
+        Ok((remaining, matched)) => println!("OK:\nRemaining input: {},\nmatched chars: {:#?}", remaining, matched),
         Err(e) => println!("ERR {}", e)
     }
 }
 
-fn c_char(c: char) -> impl Fn(&str) -> Result<(&str, ()), &str> {
-    move |input: &str| -> Result<(&str, ()), &str> {
+fn c_char(c: char) -> impl Fn(&str) -> Result<(&str, char), &str> {
+    move |input: &str| -> Result<(&str, char), &str> {
         if input.starts_with(c) {
-            Ok((&input[1..], ()))
+            Ok((&input[1..], c))
         } else {
             Err(input)
         }
     }
 }
 
-fn c_sequence<T>(parsers: Vec<T>) -> impl Fn(&str) -> Result<(&str, ()), &str>
+fn c_sequence<T>(parsers: Vec<T>) -> impl Fn(&str) -> Result<(&str, Vec<()>), &str>
 where
     T: Fn(&str) -> Result<(&str, ()), &str>,
 {
-    move |input: &str| -> Result<(&str, ()), &str> {
+    move |input: &str| -> Result<(&str, Vec<()>), &str> {
         let mut remaining = &input[..];
+        let mut matched = vec![];
         for parser in &parsers {
-            if let Ok((rem, _)) = parser(remaining) {
+            if let Ok((rem, matched_char)) = parser(remaining) {
                 remaining = rem;
+                matched.push(matched_char);
             } else {
                 return Err(input);
             }
         }
-        Ok((remaining, ()))
+        Ok((remaining, matched))
     }
 }
 
@@ -57,7 +59,7 @@ where
     }
 }
 
-fn c_string(target: &str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
+fn c_string(target: &str) -> impl Fn(&str) -> Result<(&str, Vec<()>), &str> {
     let mut parsers = vec![];
     for letter in target.chars() {
         parsers.push(c_char(letter));
@@ -65,10 +67,10 @@ fn c_string(target: &str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
     c_sequence(parsers)
 }
 
-fn c_whitespace() -> impl Fn(&str) -> Result<(&str, ()), &str> {
-    c_choice(vec![
-        c_char(' '),
-        c_char('\n'),
-        c_char('\t')
-    ])
-}
+// fn c_whitespace() -> impl Fn(&str) -> Result<(&str, ()), &str> {
+//     c_choice(vec![
+//         c_char(' '),
+//         c_char('\n'),
+//         c_char('\t')
+//     ])
+// }
