a20e7675e45517caf73742ef23aa6665c95cf367
shanec0 <connors.shane@hotmail.com>
2023-01-17 23:01:36 -0500

    added parser type alias to shorten function signatures

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 fn main() {
-    let sample = String::from("Hello, world");
+    let sample = String::from("init int x; halt");
 
+    let program = c_sequence(vec![
+        c_string("init"),
+        c_whitespace(),
+        c_choice(vec![c_string("int"), c_string("float")]),
+        c_whitespace(),
+        c_repeat(c_letter()),
+        c_char(';'),
+        c_whitespace(),
+        c_string("halt")
+    ]);
 
-    let result = letter(&sample);
+    let result = program(&sample);
 
     match result {
         Ok((remaining, matched)) => println!(
@@ -13,29 +23,32 @@ fn main() {
     }
 }
 
-fn c_char(c: char) -> impl Fn(&str) -> Result<(&str, Vec<char>), &str> {
-    move |input: &str| -> Result<(&str, Vec<char>), &str> {
-        if input.starts_with(c) {
-            Ok((&input[1..], vec![c]))
+type Parser<'a> = Box<dyn Fn(&'a str) -> Result<(&'a str, Vec<char>), &'a str> + 'a>;
+
+fn c_char<'a>(c: char) -> Parser<'a> {
+    Box::new(
+        move |input: &'a str| -> Result<(&'a str, Vec<char>), &'a str> {
+            if input.starts_with(c) {
+                Ok((&input[1..], vec![c]))
+            } else {
+                Err(input)
+            }
+        },
+    )
+}
+
+fn c_letter<'a>() -> Parser<'a> {
+    Box::new(move |input: &'a str| -> Result<(&'a str, Vec<char>), &'a str> {
+        if input.chars().next().unwrap().is_alphabetic() {
+            Ok((&input[1..], vec![input.chars().next().unwrap()]))
         } else {
             Err(input)
         }
-    }
+    })
 }
 
-fn letter(input: &str) -> Result<(&str, Vec<char>), &str> {
-    if input.chars().next().unwrap().is_alphabetic() {
-        Ok((&input[1..], vec![input.chars().next().unwrap()]))
-    } else {
-        Err(input)
-    }
-} 
-
-fn c_sequence<T>(parsers: Vec<T>) -> impl Fn(&str) -> Result<(&str, Vec<char>), &str>
-where
-    T: Fn(&str) -> Result<(&str, Vec<char>), &str>,
-{
-    move |input: &str| -> Result<(&str, Vec<char>), &str> {
+fn c_sequence<'a>(parsers: Vec<Parser<'a>>) -> Parser<'a> {
+    Box::new(move |input: &'a str| -> Result<(&'a str, Vec<char>), &'a str> {
         let mut remaining = &input[..];
         let mut matched = vec![];
         for parser in &parsers {
@@ -47,24 +60,21 @@ where
             }
         }
         Ok((remaining, matched))
-    }
+    })
 }
 
-fn c_choice<T>(parsers: Vec<T>) -> impl Fn(&str) -> Result<(&str, Vec<char>), &str>
-where
-    T: Fn(&str) -> Result<(&str, Vec<char>), &str>,
-{
-    move |input: &str| -> Result<(&str, Vec<char>), &str> {
+fn c_choice<'a>(parsers: Vec<Parser<'a>>) -> Parser<'a> {
+    Box::new(move |input: &'a str| -> Result<(&'a str, Vec<char>), &'a str> {
         for parser in &parsers {
             if let Ok(x) = parser(input) {
                 return Ok(x);
             }
         }
         Err(input)
-    }
+    })
 }
 
-fn c_string(target: &str) -> impl Fn(&str) -> Result<(&str, Vec<char>), &str> {
+fn c_string<'a>(target: &'a str) -> Parser<'a> {
     let mut parsers = vec![];
     for letter in target.chars() {
         parsers.push(c_char(letter));
@@ -72,11 +82,8 @@ fn c_string(target: &str) -> impl Fn(&str) -> Result<(&str, Vec<char>), &str> {
     c_sequence(parsers)
 }
 
-fn c_repeat<T>(parser: T) -> impl Fn(&str) -> Result<(&str, Vec<char>), &str>
-where
-    T: Fn(&str) -> Result<(&str, Vec<char>), &str>,
-{
-    move |input: &str| -> Result<(&str, Vec<char>), &str> {
+fn c_repeat<'a>(parser: Parser<'a>) -> Parser<'a> {
+    Box::new(move |input: &'a str| -> Result<(&'a str, Vec<char>), &'a str> {
         let mut remaining = &input[..];
         let mut matched = vec![];
         while let Ok((rem, mut matched_char)) = parser(remaining) {
@@ -84,9 +91,9 @@ where
             matched.append(&mut matched_char)
         }
         Ok((remaining, matched))
-    }
+    })
 }
 
-fn c_whitespace() -> impl Fn(&str) -> Result<(&str, Vec<char>), &str> {
-    c_choice(vec![c_char(' '), c_char('\n'), c_char('\t')])
+fn c_whitespace<'a>() -> Parser<'a> {
+    c_repeat(c_choice(vec![c_char(' '), c_char('\n'), c_char('\t')]))
 }
